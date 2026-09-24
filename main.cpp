f8bf392641bedbecc0fd5ed653b85c7b920af3a7
Brayden Tolle <89361210+Insanedoomed@users.noreply.github.com>
2026-09-24 12:14:25 -0500

    I updated the code. Added my portion in.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream> //required for cout and cin
-
+#include <cstdlib>
 using namespace std; //sets standard namespace
 
 //start of the main function
@@ -13,32 +13,43 @@ int main() {
     //aesthetic header
     cout <<"Welcome to the tower of the wizard of magical math tutoring (V1)" << endl;
     cout << R"(
-,---.    ,---.   ____   ,---------. .---.  .---.         .--.      .--..-./`)  ____..--'   ____    .-------.     ______
-|    \  /    | .'  __ `.\          \|   |  |_ _|         |  |_     |  |\ .-.')|        | .'  __ `. |  _ _   \   |    _ `''.
-|  ,  \/  ,  |/   '  \  \`--.  ,---'|   |  ( ' )         | _( )_   |  |/ `-' \|   .-'  '/   '  \  \| ( ' )  |   | _ | ) _  \
-|  |\_   /|  ||___|  /  |   |   \   |   '-(_{;}_)        |(_ o _)  |  | `-'`"`|.-'.'   /|___|  /  ||(_ o _) /   |( ''_'  ) |
-|  _( )_/ |  |   _.-`   |   :_ _:   |      (_,_)         | (_,_) \ |  | .---.    /   _/    _.-`   || (_,_).' __ | . (_) `. |
-| (_ o _) |  |.'   _    |   (_I_)   | _ _--.   |         |  |/    \|  | |   |  .'._( )_ .'   _    ||  |\ \  |  ||(_    ._) '
-|  (_,_)  |  ||  _( )_  |  (_(=)_)  |( ' ) |   |         |  '  /\  `  | |   |.'  (_'o._)|  _( )_  ||  | \ `'   /|  (_.\.' /
-|  |      |  |\ (_ o _) /   (_I_)   (_{;}_)|   |         |    /  \    | |   ||    (_,_)|\ (_ o _) /|  |  \    / |       .'
-'--'      '--' '.(_,_).'    '---'   '(_,_) '---'         `---'    `---` '---'|_________| '.(_,_).' ''-'   `'-'  '-----'`
+,---.    ,---.   ____   ,---------. .---.  .---.    .--.      .--..-./`)  ____..--'   ____    .-------.     ______
+|    \  /    | .'  __ `.\          \|   |  |_ _|    |  |_     |  |\ .-.')|        | .'  __ `. |  _ _   \   |    _ `''.
+|  ,  \/  ,  |/   '  \  \`--.  ,---'|   |  ( ' )    | _( )_   |  |/ `-' \|   .-'  '/   '  \  \| ( ' )  |   | _ | ) _  \
+|  |\_   /|  ||___|  /  |   |   \   |   '-(_{;}_)   |(_ o _)  |  | `-'`"`|.-'.'   /|___|  /  ||(_ o _) /   |( ''_'  ) |
+|  _( )_/ |  |   _.-`   |   :_ _:   |      (_,_)    | (_,_) \ |  | .---.    /   _/    _.-`   || (_,_).' __ | . (_) `. |
+| (_ o _) |  |.'   _    |   (_I_)   | _ _--.   |    |  |/    \|  | |   |  .'._( )_ .'   _    ||  |\ \  |  ||(_    ._) '
+|  (_,_)  |  ||  _( )_  |  (_(=)_)  |( ' ) |   |    |  '  /\  `  | |   |.'  (_'o._)|  _( )_  ||  | \ `'   /|  (_.\.' /
+|  |      |  |\ (_ o _) /   (_I_)   (_{;}_)|   |    |    /  \    | |   ||    (_,_)|\ (_ o _) /|  |  \    / |       .'
+'--'      '--' '.(_,_).'    '---'   '(_,_) '---'    `---'    `---` '---'|_________| '.(_,_).' ''-'   `'-'  '-----'`
 
                                                                                                                                  )" << endl;
 
-    //jokes
     cout <<R"(A wise wizards words:
-What's a wizard's favorite school subject? spell-gebra!
-Why did the sorcerer break his calculator? He wanted to do math-a-magics in his head!
-What is a witch's favorite shape? A Hex-agon!
-What do you call a snake that is 3.14 feet long? A Pi-thon!)"<< endl;
+    What's a wizard's favorite school subject? spell-gebra!
+    Why did the sorcerer break his calculator? He wanted to do math-a-magics in his head!
+    What is a witch's favorite shape? A Hex-agon!
+    What do you call a snake that is 3.14 feet long? A Pi-thon!)"<< endl;
     
     //obtaining user's name
     cout <<"What is your name young apprentice?"<< endl;
     cin >>name;
 
     //math question and answer
-    cout << name << ", What is " << left << "+ " << right << "= ";
+    cout <<"Ah... "<< name << "! So you have finally found me. If you truly seek the knowledge hidden within these ancient halls, you must first prove your mind is sharp." << endl;
+    cout <<"What is " << left << " + " << right << " = ";
     cin >> answer;
 
+    if (answer == 13) {
+        cout << "Good job! You Got The Question Right!" << endl;
+        // Got the answer correct and will move on.
+
+    }
+    else {
+        cout << "I'm So Disappointed In You Apprentice" << endl;
+        // They got it wrong so now they know
+    }
+    cout <<"This is all for out program! I hope you had a fanstic time!" << endl;
+    cout << "The Wizard math V2 Be Out Shortly!" << endl;
     return 0;
 }
